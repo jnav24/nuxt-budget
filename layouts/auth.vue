7c562179aa55c86e7f49bc781e8ecb6173da6365
Justin Navarro <info@justinnavarro.net>
2022-11-28 03:14:49 -0500

    started building the login page

---
+++ layouts/auth.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="min-h-screen flex flex-col justify-center items-center">
+    <div
+        class="absolute top-0 left-0 w-full h-full bg-gray-100 sm:bg-primary sm:bg-opacity-75 z-20"
+    ></div>
+    <div class="bg-image"></div>
+
+    <div
+        class="relative z-30 w-full sm:w-100 mt-6 px-4 sm:px-0 pt-4 sm:bg-white sm:rounded-lg sm:shadow-md overflow-hidden text-left"
+    >
+      <div class="flex flex-row justify-center mb-4">
+        <div class="overflow-hidden h-16 w-16">
+          <img
+              src="@/assets/images/logo_1.png"
+              alt="Dime Budget"
+              style="max-width: 12rem;"
+              class="crisp h-full w-64"
+          />
+        </div>
+      </div>
+
+      <slot />
+    </div>
+  </div>
+</template>
---
+++ pages/auth/login.vue
@@ -0,0 +1,11 @@
+<script setup>
+definePageMeta({
+    layout: 'auth',
+});
+
+useHead({ title: 'Dime Budget | Login' })
+</script>
+
+<template>
+    <h1>hello</h1>
+</template>
