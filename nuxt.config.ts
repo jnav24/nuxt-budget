1c6515bf1456a6c2ccefd252bbf7a2d2c734c53c
Justin Navarro <info@justinnavarro.net>
2023-01-13 00:35:15 -0500

    add some meta data to the nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    app: {
+        head: {
+            link: [{ rel: 'icon', href: 'https://dimebudget.app/favicon.ico' }],
+            meta: [
+                {
+                    name: 'description',
+                    content:
+                        'Dime Budget is an open sourced  budget tracker for those who desire a simple, free solution to track their everyday expenses.',
+                },
+            ],
+            title: 'Dime Budget',
+        },
     },
-  },
-})
+    css: ['~/assets/css/main.css'],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+});
