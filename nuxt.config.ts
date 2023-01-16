f7e1ae25d6b42dfd197a9f07585b756e6dd02e97
Justin Navarro <info@justinnavarro.net>
2023-01-16 09:20:16 -0500

    setup runtime config in the nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,4 +20,13 @@ export default defineNuxtConfig({
             autoprefixer: {},
         },
     },
+    runtimeConfig: {
+        DB_URL: process.env.DB_URL,
+        REDIS_HOST: process.env.REDIS_HOST ?? '',
+        REDIS_PASSWORD: process.env.REDIS_PASSWORD ?? '',
+        REDIS_PRIMARY_PORT: process.env.REDIS_PRIMARY_PORT ?? 6379,
+        public: {
+            APP_NAME: process.env.APP_NAME,
+        },
+    },
 });
