54d3536768749203e3c4d350a091c3a94b49053f
Justin Navarro <info@justinnavarro.net>
2023-01-30 09:37:15 -0500

    add redis replica to the nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,6 +25,7 @@ export default defineNuxtConfig({
         REDIS_HOST: process.env.REDIS_HOST ?? '',
         REDIS_PASSWORD: process.env.REDIS_PASSWORD ?? '',
         REDIS_PRIMARY_PORT: process.env.REDIS_PRIMARY_PORT ?? 6379,
+        REDIS_REPLICA_PORT: process.env.REDIS_REPLICA_PORT ?? 6380,
         public: {
             APP_NAME: process.env.APP_NAME,
         },
