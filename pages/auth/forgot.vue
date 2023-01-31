ee19dc5e01e17589d30f95445fb4023e7bf68978
Justin Navarro <info@justinnavarro.net>
2023-01-31 18:07:33 -0500

    add link to go back to the login page

--- pages/auth/forgot.vue
+++ pages/auth/forgot.vue
@@ -8,4 +8,9 @@ const emailSent = ref(false);
 <template>
     <FormForgot v-if="!emailSent" />
     <FormForgotSuccessful v-if="emailSent" />
+    <template #footer>
+        <NuxtLink class="text-gray-700 underline text-sm hover:no-underline" to="/auth/login">
+            Back to login
+        </NuxtLink>
+    </template>
 </template>
