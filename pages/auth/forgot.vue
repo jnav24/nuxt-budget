ef5687945eb9a008ad4768cf4c09d7322d99f09c
Justin Navarro <info@justinnavarro.net>
2023-01-31 18:09:21 -0500

    fixed the codegen slot error on the forgot page

--- pages/auth/forgot.vue
+++ pages/auth/forgot.vue
@@ -1,16 +1,17 @@
 <script setup>
-definePageMeta({ layout: 'auth' });
 useHead({ title: 'Dime Budget | Forgot Password' });
 
 const emailSent = ref(false);
 </script>
 
 <template>
-    <FormForgot v-if="!emailSent" />
-    <FormForgotSuccessful v-if="emailSent" />
-    <template #footer>
-        <NuxtLink class="text-gray-700 underline text-sm hover:no-underline" to="/auth/login">
-            Back to login
-        </NuxtLink>
-    </template>
+    <NuxtLayout name="auth">
+        <FormForgot v-if="!emailSent" />
+        <FormForgotSuccessful v-if="emailSent" />
+        <template #footer>
+            <NuxtLink class="text-gray-700 underline text-sm hover:no-underline" to="/auth/login">
+                Back to login
+            </NuxtLink>
+        </template>
+    </NuxtLayout>
 </template>
