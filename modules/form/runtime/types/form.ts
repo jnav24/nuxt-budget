952c00b64abfd4c1abd15d92c616b92ee920d8fd
Justin Navarro <info@justinnavarro.net>
2023-02-16 10:21:34 -0500

    add form types

---
+++ modules/form/runtime/types/form.ts
@@ -0,0 +1,41 @@
+export const FormContext = Symbol('FormContext');
+
+type FormElementsType = Record<string, FormElementValidationType>;
+
+export type FormContextType = {
+    formElements: FormElementsType;
+    setupForm: (label: string, rules: any) => string;
+    validateField: (labelId: string, value: string, initialize?: boolean) => string | null;
+    valid: boolean;
+    validateAllFields: () => void;
+    resetFields: () => void;
+};
+
+export type RulesOptions = {
+    message?: string;
+    pattern?: string;
+};
+
+export type RulesType = {
+    'alpha-numeric'?: RulesOptions;
+    email?: RulesOptions;
+    [key: `eq:${number}`]: RulesOptions;
+    float?: RulesOptions;
+    [key: `gt:${number}`]: RulesOptions;
+    'has-int'?: RulesOptions;
+    lower?: RulesOptions;
+    [key: `match:${string}`]: RulesOptions;
+    [key: `max:${number}`]: RulesOptions;
+    [key: `min:${number}`]: RulesOptions;
+    numeric?: RulesOptions;
+    phone?: RulesOptions;
+    required?: RulesOptions;
+    upper?: RulesOptions;
+};
+
+export type FormElementValidationType = {
+    valid: boolean;
+    rules: RulesType | Array<keyof RulesType>;
+    error: string | null;
+    value: string;
+};
