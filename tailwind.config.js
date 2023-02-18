d034d65a0323ff921eeac105b1ddf69736ea1e3d
Justin Navarro <info@justinnavarro.net>
2023-02-18 11:23:52 -0500

    add modules to the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
     content: [
         './components/**/*.{js,vue,ts}',
+        './modules/**/*.vue',
         './layouts/**/*.vue',
         './pages/**/*.vue',
         './plugins/**/*.{js,ts}',
