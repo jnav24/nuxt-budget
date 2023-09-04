60f55c0e04c6fac7664e214cb718d22d4d3b7f39
Justin Navarro <info@justinnavarro.net>
2023-09-04 14:00:53 -0400

    add new env variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,10 +29,13 @@ export default defineNuxtConfig({
     runtimeConfig: {
         APP_ENV: process.env.APP_ENV ?? 'development',
         DB_URL: process.env.DB_URL,
+        HASH_KEY: process.env.HASH_KEY,
+        ENCRYPT_SECRET: process.env.ENCRYPT_SECRET,
         REDIS_HOST: process.env.REDIS_HOST ?? '',
         REDIS_PASSWORD: process.env.REDIS_PASSWORD ?? '',
         REDIS_PRIMARY_PORT: process.env.REDIS_PRIMARY_PORT ?? '6379',
         REDIS_REPLICA_PORT: process.env.REDIS_REPLICA_PORT ?? '6380',
+        SESSION_NAME: process.env.SESSION_NAME ?? '',
         public: {
             APP_NAME: process.env.APP_NAME,
         },
