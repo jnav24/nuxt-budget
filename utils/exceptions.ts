f39813d5a498563f74af15961f2f3b819d2ff6ae
Justin Navarro <info@justinnavarro.net>
2023-09-22 10:12:29 -0400

    add a couple exceptions

--- utils/exceptions.ts
+++ utils/exceptions.ts
@@ -3,9 +3,15 @@ import { STATUS_CODES } from 'http';
 export class BaseException extends Error {
     code = STATUS_CODES[503];
     status = 503;
+}
 
+export class BadRequestException extends BaseException {
     constructor(message: string) {
         super(message);
+        const status = 400;
+        this.code = STATUS_CODES[status] ?? '';
+        this.name = 'BadRequestException';
+        this.status = status;
     }
 }
 
@@ -33,6 +39,16 @@ export class UnauthorizedException extends BaseException {
         const status = 401;
         this.code = STATUS_CODES[status] ?? '';
         this.name = 'UnauthorizedException';
+        this.status = status;
+    }
+}
+
+export class TooManyRequestsException extends BaseException {
+    constructor(message: string) {
+        super(message);
+        const status = 429;
+        this.code = STATUS_CODES[status] ?? '';
+        this.name = 'TooManyRequestsException';
         this.status = status;
     }
 }
