616d15a048a034e055cde23be5a58501200c74a7
Justin Navarro <info@justinnavarro.net>
2023-10-01 13:28:32 -0400

    update the user seeder to create a complete user

--- prisma/seeders/UserSeeder.ts
+++ prisma/seeders/UserSeeder.ts
@@ -1,5 +1,10 @@
+import { UserProfile, UserVehicles } from '@prisma/client';
 import UserFactory from '../factories/UserFactory';
+import UserProfileFactory from '../factories/UserProfileFactory';
+import UserVehicleFactory from '../factories/UserVehicleFactory';
 
 export default async function UserSeeder() {
-    await new UserFactory().count(5).verified().create();
+    const user = await new UserFactory().verified().create();
+    await new UserProfileFactory().create<UserProfile>({ user_id: user.id });
+    await new UserVehicleFactory().active().create<UserVehicles>({ user_id: user.id });
 }
