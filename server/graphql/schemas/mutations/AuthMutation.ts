f3c3533d3c3fcc468c3fd8e091a46163233057ce
Justin Navarro <info@justinnavarro.net>
2023-10-16 08:05:44 -0400

    add login functionality to the auth mutation

--- server/graphql/schemas/mutations/AuthMutation.ts
+++ server/graphql/schemas/mutations/AuthMutation.ts
@@ -1,4 +1,11 @@
 import { builder } from '~/server/graphql/builder';
+import { useDatabase } from '#budgetdb';
+import { useFormValidator } from '#budgetform';
+import { LoginInput } from '~/server/graphql/schemas/inputs/AuthInput';
+import { authenticateUser } from '~/utils/server/authenticate';
+
+const { db } = useDatabase();
+const { validateRequest } = useFormValidator();
 
 const setBuilder = (pothos: typeof builder) => {
     pothos.mutationField('logout', (t) =>
@@ -6,9 +13,33 @@ const setBuilder = (pothos: typeof builder) => {
             skipTypeScopes: true,
             type: 'Auth',
             nullable: true,
-            resolve: async (_parent, _args, { req, res }, _info) => {
+            resolve: async (_parent, _args, _context, _info) => {
                 // await removeRememberMe({ req, res });
                 // await removeSession(req);
+                return { success: true };
+            },
+        }),
+    );
+
+    pothos.mutationField('login', (t) =>
+        t.field({
+            skipTypeScopes: true,
+            type: 'Auth',
+            args: { input: t.arg({ type: LoginInput, required: true }) },
+            resolve: async (_parent, { input }, context) => {
+                validateRequest(input, { email: ['required', 'email'], password: ['required'] });
+                await authenticateUser(input, context);
+                return { mfa: false, success: true };
+            },
+        }),
+    );
+
+    pothos.mutationField('register', (t) =>
+        t.field({
+            skipTypeScopes: true,
+            type: 'Auth',
+            args: {},
+            resolve: () => {
                 return { success: true };
             },
         }),
