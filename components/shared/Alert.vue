dac9fcfcbfac308c3e0c166740192f8070f1807f
Justin Navarro <info@justinnavarro.net>
2023-10-25 06:01:21 -0400

    adjust the colors of the alert component

--- components/shared/Alert.vue
+++ components/shared/Alert.vue
@@ -10,7 +10,7 @@ withDefaults(defineProps<Props>(), { title: null });
             'border-red-600 bg-red-200 text-red-700 dark:bg-red-600 dark:text-white':
                 type === 'error',
             'border-blue=600 bg-blue-200': type === 'info',
-            'border-green-600 bg-green-600 text-green-800 dark:bg-green-600 dark:text-white':
+            'border-green-400 bg-green-300 text-green-800 dark:bg-green-600 dark:text-white':
                 type === 'success',
             'border-yellow-500 bg-yellow-200': type === 'warn',
         }"
