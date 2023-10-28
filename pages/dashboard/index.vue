a515526f0233317297a1863a82d3a7b3de26e3d4
Justin Navarro <info@justinnavarro.net>
2023-10-27 21:10:48 -0400

    created dashboard page

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { logout } = useGQLAuth();
+const router = useRouter();
+
+const logUserOut = async () => {
+    const response = await logout.mutate();
+
+    if (response?.data) {
+        router.push({ path: '/auth/login' });
+    }
+};
+</script>
+
+<template>
+    <NuxtLayout name="dashboard">
+        <h1>Dashboard</h1>
+        <button @click="logUserOut()" type="button">Logout</button>
+    </NuxtLayout>
+</template>
