a53012aaa29c86716984dd0ab616c1f3fa6a3db4
Justin Navarro <info@justinnavarro.net>
2023-10-30 09:37:34 -0400

    created card content component

---
+++ components/shared/card/Content.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { CardProvider } from '~/providers/CardProvider';
+const { setCardContent } = inject(CardProvider);
+onMounted(() => setCardContent('content'));
+</script>
+
+<template>
+    <div class="px-4">
+        <slot></slot>
+    </div>
+</template>
