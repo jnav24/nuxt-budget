a94c65b3b914fe851c4473e87481b7da64e2713a
Justin Navarro <info@justinnavarro.net>
2023-10-30 09:00:35 -0400

    created card container

---
+++ components/shared/card/Container.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+export const CardProvider = Symbol('Card Provider');
+
+type Props = {
+    color?: string;
+};
+
+withDefaults(defineProps<Props>(), { color: 'white' });
+
+const cardContents: Record<string, boolean> = reactive({
+    content: false,
+    footer: false,
+    header: false,
+});
+
+const setCardContent = (type: string) => {
+    if ({}.propertyIsEnumerable.call(cardContents, type)) {
+        cardContents[type] = true;
+    }
+};
+
+provide(CardProvider, { setCardContent });
+</script>
+
+<template>
+    <div
+        class="my-2 rounded-md shadow-sm transition duration-150 sm:my-4"
+        :class="{
+            [`bg-${color}`]: true,
+            'px-4': !cardContents.content,
+            'pb-4': !cardContents.footer,
+            'pt-4': !cardContents.header,
+        }"
+    >
+        <!-- transform hover:scale-105 hover:text-white -->
+        <slot></slot>
+    </div>
+</template>
