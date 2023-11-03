f7adb9caf4c2d4b30d580e66df4880c38b5fefd2
Justin Navarro <info@justinnavarro.net>
2023-11-03 12:39:06 -0400

    created card footer

---
+++ components/shared/card/Footer.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { CardProvider } from '~/providers/CardProvider';
+const { setCardContent } = inject(CardProvider);
+onMounted(() => setCardContent('footer'));
+</script>
+
+<template>
+    <div class="px-4 pb-4">
+        <slot></slot>
+    </div>
+</template>
