17313242199419ebbee0e5ec42ab53da3afc52f8
Justin Navarro <info@justinnavarro.net>
2023-11-03 12:39:16 -0400

    created card header

---
+++ components/shared/card/Header.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { CardProvider } from '~/providers/CardProvider';
+const { setCardContent } = inject(CardProvider);
+onMounted(() => setCardContent('header'));
+</script>
+
+<template>
+    <div class="px-4 py-4">
+        <slot></slot>
+    </div>
+</template>
