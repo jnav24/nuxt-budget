46413283cd9629275e0f851454ffab1b83161543
Justin Navarro <info@justinnavarro.net>
2023-11-04 13:39:29 -0400

    created update password section component

---
+++ components/settings/UpdatePassword.vue
@@ -0,0 +1,9 @@
+<template>
+    <NuxtLayout
+        name="settings"
+        title="Update Password"
+        description="Ensure your account is using a long, random password to stay secure."
+    >
+        <FormSettingUpdatePassword />
+    </NuxtLayout>
+</template>
