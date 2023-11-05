537bb98e618998c0e578fbe9e567d567133a1c8b
Justin Navarro <info@justinnavarro.net>
2023-11-05 08:10:57 -0500

    add prop to dashboard layout add/remove top padding

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,8 +1,16 @@
+<script setup lang="ts">
+type Props = {
+    noTopPadding?: boolean;
+};
+
+defineProps<Props>();
+</script>
+
 <template>
     <DashboardNav />
 
     <div class="h-screen overflow-y-auto bg-gray-200 pb-32">
-        <div class="container mx-auto">
+        <div class="container mx-auto" :class="{ 'pt-8': !noTopPadding }">
             <slot />
         </div>
     </div>
