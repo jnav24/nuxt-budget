c153fba9fa23009c348e61256e6be0f08865e0a9
Justin Navarro <info@justinnavarro.net>
2023-11-05 08:00:55 -0500

    add two factor component

---
+++ components/settings/TwoFactor.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const toggleState = ref(false);
+</script>
+
+<template>
+    <NuxtLayout
+        name="settings"
+        title="Two Factor Authentication"
+        description="Add additional security to your account by using two factor authentication"
+    >
+        <div class="mb-2 flex flex-row items-center justify-between text-gray-700">
+            <span>{{ toggleState ? 'Enabled' : 'Disabled' }}</span>
+            <BudgetToggle v-model:value="toggleState" />
+        </div>
+    </NuxtLayout>
+</template>
