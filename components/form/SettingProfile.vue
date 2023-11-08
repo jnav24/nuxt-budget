3fa982799533062ebe8f5c3a8bc96e4217684af2
Justin Navarro <info@justinnavarro.net>
2023-11-08 06:11:53 -0500

    on the settings profile page, pre-populate the user profile fields

--- components/form/SettingProfile.vue
+++ components/form/SettingProfile.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+const { userData } = useGQLUser();
+
 const isSuccess = ref(false);
 const showAlert = ref(false);
 const valid = ref(false);
@@ -7,6 +9,18 @@ const form = reactive({
     email: '',
     firstName: '',
     lastName: '',
+});
+
+watchEffect(() => {
+    if (userData.value) {
+        form.firstName = userData.value?.profile.firstName;
+        form.lastName = userData.value?.profile.lastName;
+        form.email = userData.value?.email;
+        // @note this is so strange
+        // if I uncomment the line below, the form will NOT populate with saved data
+        // If I comment the line below, the form will populate but the submit button will stay disabled
+        // valid.value = true;
+    }
 });
 
 const handleSave = () => {};
@@ -26,7 +40,12 @@ const handleSave = () => {};
             :rules="['required', 'min:3']"
         />
 
-        <BudgetInput v-model:value="form.email" label="Email" :rules="['required', 'email']" />
+        <BudgetInput
+            v-model:value="form.email"
+            label="Email"
+            :rules="['required', 'email']"
+            read-only
+        />
 
         <SharedCardFooter class="flex flex-row items-center justify-end !pr-0">
             <SharedInlineAlert v-model:show="showAlert" :is-success="isSuccess" />
